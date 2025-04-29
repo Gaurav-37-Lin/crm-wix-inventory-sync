1e6205542baccb1dda8eeb33d59caaed7c35aceb
Gaurav-37-Lin <gaurav.dubey@rentmax.in>
2025-04-29 20:38:07 +0530

    new changes

--- controllers/addInventory.js
+++ controllers/addInventory.js
@@ -5,21 +5,17 @@ exports.addOrUpdateInventory = async (req, res) => {
     const {
       Listing_Id,
       Project_Name,
-      Wing_Name,
-      Society_Sector_Name,
       Configuration,
       Monthly_Rent,
       If_Security_Deposit_is_lumpsum
     } = req.body;
 
     const inventoryData = {
-      listing_id: Listing_Id,
-      project_name: Project_Name,
-      wing_name: Wing_Name,
-      society_name: Society_Sector_Name,
-      configuration: Configuration,
-      monthly_rent: Monthly_Rent,
-      security_deposit: If_Security_Deposit_is_lumpsum
+      listingId: Listing_Id,
+      projectName: Project_Name,
+      apartmentConfiguration: Configuration,
+      monthlyRent: Monthly_Rent,
+      securityDeposit: If_Security_Deposit_is_lumpsum
     };
 
     await wixCMSService.upsertInventory(inventoryData);
